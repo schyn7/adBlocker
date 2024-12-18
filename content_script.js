9af00b7dea353cd74f74055d28b1e56951ab2fc0
schyn7 <64598144+schyn7@users.noreply.github.com>
2024-12-18 23:00:12 +0530

    Add files via upload

---
+++ content_script.js
@@ -0,0 +1,79 @@
+﻿console.log("AdBlocker Started");
+
+function ad_elements() {
+
+    skip = document.getElementsByClassName("ytp-ad-skip-button-container")[0].click();
+    skip.onclick = console.log('Ad_skiped')
+
+
+    }
+var interval = setInterval(ad_elements, 500);
+interval;
+
+/*setInterval( function() {
+       var myNode = document.querySelector("#player-ads");
+          while (myNode.firstChild) {
+              myNode.removeChild(myNode.firstChild);}
+       var ad_closed = document.getElementsByClassName("ytp-ad-overlay-close-button")[0].click();
+        ad_closed.onclick = console.log('Ad_closed')
+
+
+    }
+    }, 1000)
+    */
+function home() {
+    document.getElementById("masthead-ad").style.display ='none'
+    }
+var gonn = setInterval(home, 1000)
+gonn;
+
+
+function Ad() {
+       var myNode = document.querySelector("#player-ads");
+           while (myNode.firstChild) {
+               myNode.removeChild(myNode.firstChild);
+           }
+       Ad_closed = document.getElementsByClassName("ytp-ad-overlay-close-button")[0].click();
+       Ad_closed.onclick = console.log('Ad_closed')
+
+    }
+
+
+
+var time = setInterval(Ad, 1000)
+time;
+
+var p = setInterval(function (){
+    console.clear(Error)
+},10000)
+p;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
